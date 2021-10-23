a31c4076529b52b3a669155b54ab2afaa02e61c6
Beben Sutara, S.Kom, MT <58406018+bensu89@users.noreply.github.com>
2021-10-23 11:26:52 +0700

    Delete Bab 09 - Laravel MIX directory

--- Bab 09 - Laravel MIX/my-script-console.js
+++
@@ -1,5 +0,0 @@
-// Tampilkan pesan console ketika halaman di klik
-
-window.addEventListener('click',function(){
-    console.log('Saya juga di klik');
-});
--- Bab 09 - Laravel MIX/my-script.js
+++
@@ -1,5 +0,0 @@
-// Tampilkan alert ketika halaman di klik
-
-window.addEventListener('click',function(){
-    alert('Saya di klik');
-});
