64e1c19fc3c7b31fc3c4901f5d20e3e999325272
Beben Sutara, S.Kom, MT <58406018+bensu89@users.noreply.github.com>
2021-11-02 09:59:31 +0700

    Delete Bab 11 - Controller/01. Membuat Controller/02. Cara Penulisan Route directory

--- Bab 11 - Controller/01. Membuat Controller/02. Cara Penulisan Route/route/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
-
-Route::get('/', [PageController::class,'index']);
-Route::get('/mahasiswa',[PageController::class,'tampil']);
