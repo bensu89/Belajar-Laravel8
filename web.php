6e0ee888d40cfc5c18342b3f0bcba76f9f4803d6
Beben Sutara, S.Kom, MT <58406018+bensu89@users.noreply.github.com>
2021-11-16 12:24:26 +0700

    Add files via upload

--- web.php
+++ web.php
@@ -1,24 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
-
-// ====================
-// ## Route dengan Optional Parameter ##
-
-Route::get('/stok_barang/{jenis?}/{merek?}',
-    function ($a = 'smartphone',$b = 'samsung') {
-    return "Cek sisa stok untuk $a $b";
-});
+Route::get('/',                [MahasiswaController::class,'index']);
+Route::get('/insert-sql',      [MahasiswaController::class,'insertSql']);
+Route::get('/insert-timestamp',[MahasiswaController::class,'insertTimestamp']);
+Route::get('/insert-prepared', [MahasiswaController::class,'insertPrepared']);
+Route::get('/insert-binding',  [MahasiswaController::class,'insertBinding']);
+Route::get('/update',          [MahasiswaController::class,'update']);
+Route::get('/delete',          [MahasiswaController::class,'delete']);
+Route::get('/select',          [MahasiswaController::class,'select']);
+Route::get('/select-tampil',   [MahasiswaController::class,'selectTampil']);
+Route::get('/select-view',     [MahasiswaController::class,'selectView']);
+Route::get('/select-where',    [MahasiswaController::class,'selectWhere']);
+Route::get('/statement',       [MahasiswaController::class,'statement']);
