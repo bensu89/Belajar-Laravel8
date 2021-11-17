a52b7d05ae708b2c9a959a6f95f63e984401ec02
Beben Sutara, S.Kom, MT <58406018+bensu89@users.noreply.github.com>
2021-11-17 17:43:40 +0700

    Create index-mahasiswa.blade.php

---
+++ Bab 15 - Query Builder/views/index-mahasiswa.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="{{ asset('/css/bootstrap.min.css')}} " rel="stylesheet">
+  <title>Data Mahasiswa</title>
+</head>
+<body>
+
+<div class="container text-center p-4">
+  <h1>Mahasiswa</h1>
+  <div class="row">
+    <div class="col-sm-8 col-md-6 m-auto">
+      <ol class="list-group">
+        @forelse ($mahasiswas as $mahasiswa)
+          <li class="list-group-item">
+            <a href="{{ url('mahasiswa/'.$mahasiswa->nim)}}">
+	  	      {{ $loop->iteration }}. {{$mahasiswa->nama}}
+	  	    </a>
+          </li>
+        @empty
+          <div class="alert alert-dark d-inline-block">Tidak ada data...</div>
+        @endforelse
+      </ol>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="{{ asset('/css/bootstrap.min.css')}} " rel="stylesheet">
+  <title>Data Mahasiswa</title>
+</head>
+<body>
+
+<div class="container text-center p-4">
+  <h1>Data Mahasiswa</h1>
+  <div class="row">
+    <div class="m-auto">
+      <ol class="list-group">
+        @forelse ($mahasiswas as $mahasiswa)
+          <li class="list-group-item">
+            {{$mahasiswa->nama}} ( {{$mahasiswa->nim}} ),
+            Tanggal Lahir: {{$mahasiswa->tanggal_lahir}},
+            IPK: {{$mahasiswa->ipk}}
+          </li>
+        @empty
+          <div class="alert alert-dark d-inline-block">Tidak ada data...</div>
+        @endforelse
+      </ol>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
