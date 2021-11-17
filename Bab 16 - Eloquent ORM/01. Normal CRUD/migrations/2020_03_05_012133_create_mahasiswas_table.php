2764b5ab38c8099607cd0b53a4cab8d5e34b4221
Beben Sutara, S.Kom, MT <58406018+bensu89@users.noreply.github.com>
2021-11-17 17:51:21 +0700

    Create 2020_03_05_012133_create_mahasiswas_table.php

---
+++ Bab 16 - Eloquent ORM/01. Normal CRUD/migrations/2020_03_05_012133_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->char('nim',8)->unique();
+            $table->string('nama');
+            $table->date('tanggal_lahir');
+            $table->decimal('ipk',3,2)->default(1.00);       
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
