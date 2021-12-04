b6ed4f4dbd7e2585fef192b55181de2cbf36e038
Beben Sutara, S.Kom, MT <58406018+bensu89@users.noreply.github.com>
2021-12-04 22:53:55 +0700

    Add files via upload

---
+++ File Upload/05. Membuat Symlink/controllers/FileUploadController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileUploadController extends Controller
+{
+    public function fileUpload(){
+        return view('file-upload');
+    }
+
+    public function prosesFileUpload(Request $request){
+        $request->validate([
+             'berkas'  => 'required|file|image|max:1000',
+        ]);
+
+        # Nama file upload di generate dari nama user + time() seperti nama file asal
+        $extFile = $request->berkas->getClientOriginalExtension();
+        $namaFile = 'lisa-'.time().".".$extFile;
+        # Pindahkan file upload ke public
+        $path = $request->berkas->storeAs('public',$namaFile);
+
+        // echo "Proses upload berhasil, file berada di: $path";
+
+        // echo "Proses upload berhasil, file berada di: storage/$namaFile";
+
+        $pathBaru = asset('storage/'.$namaFile);
+        echo "Proses upload berhasil, file berada di: <a href='$pathBaru'>$pathBaru</a>";
+    }
+}
