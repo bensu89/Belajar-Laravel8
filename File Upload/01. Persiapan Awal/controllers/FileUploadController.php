f109b4569f465b3a7f008b41adcf564ade31fa4b
Beben Sutara, S.Kom, MT <58406018+bensu89@users.noreply.github.com>
2021-12-04 22:49:38 +0700

    Create FileUploadController.php

---
+++ File Upload/01. Persiapan Awal/controllers/FileUploadController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FileUploadController extends Controller
+{
+    public function fileUpload(){
+        return view('file-upload');
+    }
+
+    public function prosesFileUpload(Request $request){
+        return "Pemrosesan file upload di sini";
+    }
+}
