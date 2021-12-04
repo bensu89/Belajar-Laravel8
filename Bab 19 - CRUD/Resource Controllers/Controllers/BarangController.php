0079e872ccfb5e90c05e5e560023da107c5bcfb4
Beben Sutara, S.Kom, MT <58406018+bensu89@users.noreply.github.com>
2021-12-04 22:47:38 +0700

    Add files via upload

---
+++ Bab 19 - CRUD/Resource Controllers/Controllers/BarangController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barang;
+use Illuminate\Http\Request;
+
+class BarangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Barang  $barang
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Barang $barang)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Barang  $barang
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Barang $barang)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Barang  $barang
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Barang $barang)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Barang  $barang
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Barang $barang)
+    {
+        //
+    }
+}
