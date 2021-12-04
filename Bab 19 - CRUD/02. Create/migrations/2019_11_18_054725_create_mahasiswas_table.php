f01bd24d83d193cf7cff316d3f61d51bb812c1d7
Beben Sutara, S.Kom, MT <58406018+bensu89@users.noreply.github.com>
2021-12-04 22:30:02 +0700

    Add files via upload

---
+++ Bab 19 - CRUD/02. Create/migrations/2019_11_18_054725_create_mahasiswas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->id();
+            $table->char('nim',8)->unique();
+            $table->string('nama');
+            $table->char('jenis_kelamin',1);
+            $table->string('jurusan');
+            $table->text('alamat')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+}
